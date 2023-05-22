d0547717c67bd2b5c3bd36da25598bb18173bfc5
Steveb599 <Steveb599@gmail.com>
2023-05-22 19:29:49 +0300

    toy-index

--- frontend/src/views/toy-index.jsx
+++ frontend/src/views/toy-index.jsx
@@ -1,18 +1,60 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { loadToys } from '../store/toy.action.js'
+import { loadToys, removeToy, saveToy } from '../store/toy.action.js'
+import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service.js'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { toyService } from '../services/toy.service.js'
 
 
 export function ToyIndex() {
     const toys = useSelector(state => state.toyModule.toys)
+    // const [filterBy, setFilterBy] = useState(carService.getDefaultFilter())
+    const dispatch = useDispatch()
+    // const [sortBy, setSortBy] = useState({ type: '', desc: 1 })
 
 
     useEffect(() => {
+        // add here sortby, filterby when we get there
         loadToys()
     }, [])
 
+    function onRemoveToy(toyId) {
+        removeToy(toyId)
+            .then(() => {
+                showSuccessMsg('Toy removed')
+            })
+            .catch(err => {
+                showErrorMsg('Cannot remove toy')
+            })
+    }
+
+    // function onAddToy() {
+    //     const toyToSave = carService.getEmptyToy()
+    //     saveCar(toyToSave)
+    //         .then((savedCar) => {
+    //             showSuccessMsg(`Car added (id: ${savedCar._id})`)
+    //         })
+    //         .catch(err => {
+    //             showErrorMsg('Cannot add car')
+    //         })
+    // }
+
+    // function onSetFilter(filterBy) {
+    //     console.log('FilterBy', filterBy)
+    //     setFilterBy(filterBy)
+    // }
+
     return (
         <section className="toy-index">
-            <h2 className="text-align-center">hello from index</h2>
-        </section>
+            <Link to={`/toy/edit`}>Add Car</Link>
+            {/* <CarFilter onSetFilter={onSetFilter} /> */}
+            {isLoading && <h4>Loading...</h4>}
+            <ToyList
+                toys={toys}
+                // sortBy={sortBy}
+                // setSortBy={setSortBy}
+                onRemoveToy={onRemoveToy}
+            />
+        </section >
     )
 }
