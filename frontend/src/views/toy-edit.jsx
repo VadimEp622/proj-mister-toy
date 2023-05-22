e83602687ca21222747f9be1dd4ae518129a3945
Vlad Epov <vladep622@gmail.com>
2023-05-22 20:29:24 +0300

    built toy-edit

--- frontend/src/views/toy-edit.jsx
+++ frontend/src/views/toy-edit.jsx
@@ -1,10 +1,12 @@
-import { toyService } from "../services/toy.service.js"
-
-// import { UPDATE_IS_EDIT } from "../store/toy.reducer.js"
-// import { saveToy } from "../store/toy.actions.js"
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
+
 import { showErrorMsg } from "../services/event-bus.service.js"
+import { saveToy } from "../store/toy.action.js"
+import { toyService } from "../services/toy.service.js"
+
+
+
 
 
 export function ToyEdit() {
@@ -27,39 +29,70 @@ export function ToyEdit() {
             })
     }
 
-
     function handleChange({ target }) {
         console.log('target.value', target.value)
         const field = target.name
-        const value = target.type === 'number' ? (+target.value || '') : target.value
+        const value = (target.type === 'number') ? (+target.value || '') :
+            (target.type === 'checkbox') ? (target.checked === 'true') :
+                target.value
         setToyToEdit(prevToyEdit => ({ ...prevToyEdit, [field]: value }))
     }
 
-    // function handleCheckBoxChange({ target }, str) {
-    //     labels.includes(str)
-    // }
-
     function handleAddToy(ev) {
         ev.preventDefault()
-        // saveToy(toyToEdit)
+        saveToy(toyToEdit)
     }
 
     const { labels, name, price, inStock } = toyToEdit
     return (
         <section className="toy-edit">
             <form action="">
-                <input type="text" name="text" placeholder="Enter toy name" value={name} onChange={handleChange} ></input>
-                <input required onChange={handleChange} value={price} type="number" name={price} placeholder="Enter price" />
-                <input type="checkbox" name={inStock} value={inStock} >in stock</input>
+                <article>
+                    <label>Name:</label>
+                    <input type="text" name="text" placeholder="Enter toy name" value={name} onChange={handleChange} />
+                </article>
+                <article>
+                    <label>Price:</label>
+                    <input required onChange={handleChange} value={price} type="number" name={price} placeholder="Enter price" />
+                </article>
+                <article>
+                    <input type="checkbox" name="isStock" value={inStock} />
+                    <label>Toy In Stock</label>
+                </article>
                 <section className="labels-input">
-                    <input type="checkbox" name="On wheels" checked={labels.includes('On wheels')} > On wheels</input>
-                    <input type="checkbox" name="Box game" checked={labels.includes('Box game')} > Box game</input>
-                    <input type="checkbox" name="Art" checked={labels.includes('Art')} > Art</input>
-                    <input type="checkbox" name="Baby" checked={labels.includes('Baby')} > Baby</input>
-                    <input type="checkbox" name="Doll" checked={labels.includes('Doll')} > Doll</input>
-                    <input type="checkbox" name="Puzzle" checked={labels.includes('Puzzle')} > Puzzle</input>
-                    <input type="checkbox" name="Outdoor" checked={labels.includes('Outdoor')} > Outdoor</input>
-                    <input type="checkbox" name="Battery Powered" checked={labels.includes('Battery Powered')} > Battery Powered</input>
+                    <h4>Labels:</h4>
+                    <article>
+                        <input type="checkbox" name="On wheels" value="On wheels" checked={labels.includes('On wheels')} onChange={handleChange} />
+                        <label>On wheels</label>
+                    </article>
+                    <article>
+                        <input type="checkbox" name="Box game" value="Box game" checked={labels.includes('Box game')} onChange={handleChange} />
+                        <label>Box game</label>
+                    </article>
+                    <article>
+                        <input type="checkbox" name="Art" value="Art" checked={labels.includes('Art')} onChange={handleChange} />
+                        <label>Art</label>
+                    </article>
+                    <article>
+                        <input type="checkbox" name="Baby" value="Baby" checked={labels.includes('Baby')} onChange={handleChange} />
+                        <label>Baby</label>
+                    </article>
+                    <article>
+                        <input type="checkbox" name="Doll" value="Doll" checked={labels.includes('Doll')} onChange={handleChange} />
+                        <label>Doll</label>
+                    </article>
+                    <article>
+                        <input type="checkbox" name="Puzzle" value="Puzzle" checked={labels.includes('Puzzle')} onChange={handleChange} />
+                        <label>Puzzle</label>
+                    </article>
+                    <article>
+                        <input type="checkbox" name="Outdoor" value="Outdoor" checked={labels.includes('Outdoor')} onChange={handleChange} />
+                        <label>Outdoor</label>
+                    </article>
+                    <article>
+                        <input type="checkbox" name="Battery Powered" value="Battery Powered" checked={labels.includes('Battery Powered')} onChange={handleChange} />
+                        <label>Battery Powered</label>
+                    </article>
                 </section>
             </form>
             <button onClick={(ev) => handleAddToy(ev)}>Add</button>
