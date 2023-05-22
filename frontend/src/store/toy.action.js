bf9b538d992490c2df20b9886b90af884f1f6fa8
Steveb599 <Steveb599@gmail.com>
2023-05-22 21:54:53 +0300

    sorting is done

--- frontend/src/store/toy.action.js
+++ frontend/src/store/toy.action.js
@@ -3,8 +3,8 @@ import { store } from './store.js'
 import { SET_TOYS, ADD_TOY, REMOVE_TOY, UPDATE_TOY } from './toy.reducer.js'
 
 
-export function loadToys(filterBy) {
-    return toyService.query(filterBy)
+export function loadToys(filterBy, sortBy) {
+    return toyService.query(filterBy, sortBy)
         .then(toys => {
             store.dispatch({ type: SET_TOYS, toys })
             return
