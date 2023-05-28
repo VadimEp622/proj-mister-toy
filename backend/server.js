d7a13985825ada4124b6b3b2db2dc07aa19cad97
Vlad Epov <vladep622@gmail.com>
2023-05-28 13:31:44 +0300

    fixed server.js for render

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
     const corsOptions = {
-        origin: ['http://127.0.0.1:3030', 'http://localhost:3030'],
+        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
         credentials: true
     }
     app.use(cors(corsOptions))
@@ -179,19 +179,16 @@ app.post('/api/auth/logout', (req, res) => {
 
 
 
-
-// Listen will always be the last line in our server!
-// app.get('/**', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
-// app.listen(3030, () => console.log('Server listening on port 3030!'))
-
-const port = process.env.PORT || 3030
-
 app.get('/**', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+// Listen will always be the last line in our server!
 
+// Instead Of This:
+// app.listen(3030, () => console.log('Server listening on port 3030!'))
+
+// This:
+const port = process.env.PORT || 3030
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`)
     console.log('Hello render.com')
